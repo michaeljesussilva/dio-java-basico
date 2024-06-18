5c4595e26be904fa943ff729c42e9b006a793c29
Michael Silva <silvamichaeljesus@gmail.com>
2024-06-17 22:39:36 -0300

    Introdução a Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
+
+        //double numero1 = 10.5;
+
+        //int numero2 = 10;
+
+        double resultado = (10*7) + (20/4);
+
+
+        System.out.println(resultado);
+        
         
     }
 }
